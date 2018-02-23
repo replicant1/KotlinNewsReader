5a7e430e0790a3ea13020a6b57cfb1564e794a05
rodbailey <rodney.p.bailey@gmail.com>
2018-02-23 22:06:55 +1100

    Added AndroidAnnotations to build. Made MainActivity an @EActivity.

--- app/src/main/java/com/bailey/rod/kotlinnewsreader/MainActivity.kt
+++ app/src/main/java/com/bailey/rod/kotlinnewsreader/MainActivity.kt
@@ -2,11 +2,20 @@ package com.bailey.rod.kotlinnewsreader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.androidannotations.annotations.EActivity
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+@EActivity(R.layout.activity_main)
+open class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+
+		// This logging facility needs initialization. Log messages
+		// only appear in DEBUG builds, not PRODUCTION builds.
+		if (BuildConfig.DEBUG) {
+			Timber.plant(Timber.DebugTree())
+			Timber.i("KotlinNewsReader started")
+		}
 	}
 }
