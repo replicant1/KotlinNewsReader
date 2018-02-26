2ecc83d2f7a5c15e3935ee4db6ccd877e56d85d1
rodbailey <rodney.p.bailey@gmail.com>
2018-02-26 22:36:32 +1100

    Incorporated ButterKnife and use it in NewsAssetViewHolder for view binding.

--- app/src/main/java/com/bailey/rod/kotlinnewsreader/ui/NewsAssetViewHolder.kt
+++ app/src/main/java/com/bailey/rod/kotlinnewsreader/ui/NewsAssetViewHolder.kt
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.bailey.rod.kotlinnewsreader.R
 import com.bailey.rod.kotlinnewsreader.app.GlideApp
 import com.bailey.rod.kotlinnewsreader.data.NewsAssetDAO
@@ -17,12 +19,24 @@ import timber.log.Timber
  */
 class NewsAssetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+	@BindView(R.id.tv_news_asset_headline)
+	lateinit var headlineTextView: TextView
+
+	@BindView(R.id.tv_news_asset_byline)
+	lateinit var bylineTextView: TextView
+
+	@BindView(R.id.tv_news_asset_abstract)
+	lateinit var abstractTextView: TextView
+
+	@BindView(R.id.iv_news_asset_thumbnail)
+	lateinit var thumbnailImageView: ImageView
+
+	init {
+		ButterKnife.bind(this, itemView)
+	}
+
 	fun bind(item: NewsAssetDAO, clickListener: INewsAssetClickListener) {
 		// Copy item data into text fields for headline, byline and abstract
-		// TODO: Do findViewById's in constructor just once (performance)
-		val headlineTextView: TextView = itemView.findViewById<TextView>(R.id.tv_news_asset_headline)
-		val bylineTextView: TextView = itemView.findViewById<TextView>(R.id.tv_news_asset_byline)
-		val abstractTextView: TextView = itemView.findViewById<TextView>(R.id.tv_news_asset_abstract)
 		headlineTextView.text = item.headline
 		bylineTextView.text = item.byline
 		abstractTextView.text = item.abstract
@@ -31,9 +45,9 @@ class NewsAssetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 		// screen that shows the full news article inside a web view.
 		itemView.setOnClickListener { clickListener.onNewsAssetClick(item) }
 
+		// TODO: Clear any current loading even if there is no replacement thumbnail
 		val potentialThumbnails = item.relatedImages
 		if (potentialThumbnails != null) {
-			val thumbnailImageView: ImageView = itemView.findViewById<ImageView>(R.id.iv_news_asset_thumbnail)
 			val thumbnail = SimpleThumbnailSelectionStrategy().selectThumbnail(potentialThumbnails)
 
 			// Cancel any outstanding Glide loads into the image view, then start a load of the thumbnail image
